8eb1441b9cb4ba55c43e40e17931f99bf662e764
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-08-07 17:07:39 -0700

    main.cpp
    SDL3 works and creates a window!!!! There are so many more components we need to implement, given that we are essentially package managing ourselves.
    
    Next will be openGL implementation so we can use the openGL renderer

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 #include <boost/program_options.hpp>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h> 
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_render.h>
 
-using namespace std;
-
-int main()
+int main(int argc, char* argv[]) 
 {
-    cout << "Dockerfile Test!!" << endl;
+
+    SDL_Window *window;
+    bool done = false;
+
+    SDL_Init(SDL_INIT_VIDEO); 
+    window = SDL_CreateWindow(
+        "An SDL3 window",
+        640,
+        480,
+        0 
+            ); 
+
+    
+    if (window == NULL) 
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    while (!done) 
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) 
+        {
+            if (event.type == SDL_EVENT_QUIT) 
+            {
+                done = true;
+            }
+        }
+
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
