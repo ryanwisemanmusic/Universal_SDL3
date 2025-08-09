94a6bfc0ff6631003ab9a30b4a625cc7281c4318
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-08-08 21:10:45 -0700

    main.hpp
    This is our testing ground for library headers. So far, these all work, which means that we will eventually break these up into their own file. We just have them living here until I can verify all my core libraries work as they should

--- main.hpp
+++ main.hpp
@@ -1,15 +1,154 @@
 #include <iostream>
 #include <boost/program_options.hpp>
+
+//==================================================
+// Foundational Headers - SDL (General)
+//==================================================
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_events.h> 
-#include <SDL3/SDL_video.h>
-#include <GL/gl.h>
+#include <SDL3/SDL_events.h>
 
-//Headers - successful import 01
-#include <SDL3/SDL_assert.h>
-#include <SDL3/SDL_asyncio.h>
-#include <SDL3/SDL_atomic.h>
+//==================================================
+// Foundational Headers - SDL (Audio/Video)
+//==================================================
 #include <SDL3/SDL_audio.h>
-#include <SDL3/SDL_begin_code.h>
+#include <SDL3/SDL_video.h>
 
-//This is a testing ground for new headers
+//==================================================
+// Foundational Headers - SDL (Core/Code Structure)
+//==================================================
+#include <SDL3/SDL_begin_code.h>
+#include <SDL3/SDL_close_code.h>
+#include <SDL3/SDL_process.h>
+#include <SDL3/SDL_revision.h>
+#include <SDL3/SDL_stdinc.h>
+
+//==================================================
+// Foundational Headers - SDL (Clipboard & Licensing)
+//==================================================
+#include <SDL3/SDL_clipboard.h>
+#include <SDL3/SDL_copying.h>
+
+//==================================================
+// Foundational Headers - SDL (Renderer Backends)
+//==================================================
+#include <SDL3/SDL_egl.h>
+#include <SDL3/SDL_gpu.h>
+#include <SDL3/SDL_opengl.h>
+#include <SDL3/SDL_metal.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_vulkan.h>
+
+//==================================================
+// Foundational Headers - SDL (Rendering Helpers)
+//==================================================
+#include <SDL3/SDL_blendmode.h>
+#include <SDL3/SDL_camera.h>
+#include <SDL3/SDL_pixels.h>
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_surface.h>
+
+//==================================================
+// Foundational Headers - SDL (OpenGL Extra Support)
+//==================================================
+#include <SDL3/SDL_opengles.h>
+#include <SDL3/SDL_opengles2.h>
+#include <SDL3/SDL_opengles2_gl2.h>
+#include <SDL3/SDL_opengles2_gl2ext.h>
+#include <SDL3/SDL_opengles2_gl2platform.h>
+#include <SDL3/SDL_opengles2_khrplatform.h>
+#include <SDL3/SDL_opengl_glext.h>
+
+//==================================================
+// Foundational Headers - SDL (System & Info)
+//==================================================
+#include <SDL3/SDL_assert.h>
+#include <SDL3/SDL_cpuinfo.h>
+#include <SDL3/SDL_hints.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_locale.h>
+#include <SDL3/SDL_log.h>
+#include <SDL3/SDL_messagebox.h>
+#include <SDL3/SDL_properties.h>
+#include <SDL3/SDL_system.h>
+#include <SDL3/SDL_version.h>
+
+//==================================================
+// Foundational Headers - SDL (File & I/O)
+//==================================================
+#include <SDL3/SDL_asyncio.h>
+#include <SDL3/SDL_filesystem.h>
+#include <SDL3/SDL_iostream.h>
+#include <SDL3/SDL_storage.h>
+
+//==================================================
+// Foundational Headers - SDL (Input Devices)
+//==================================================
+#include <SDL3/SDL_gamepad.h>
+#include <SDL3/SDL_guid.h>
+#include <SDL3/SDL_haptic.h>
+#include <SDL3/SDL_hidapi.h>
+#include <SDL3/SDL_joystick.h>
+#include <SDL3/SDL_keyboard.h>
+#include <SDL3/SDL_keycode.h>
+#include <SDL3/SDL_mouse.h>
+#include <SDL3/SDL_pen.h>
+#include <SDL3/SDL_scancode.h>
+#include <SDL3/SDL_touch.h>
+
+//==================================================
+// Foundational Headers - SDL (Hardware Access)
+//==================================================
+#include <SDL3/SDL_bits.h>
+#include <SDL3/SDL_intrin.h>
+#include <SDL3/SDL_power.h>
+#include <SDL3/SDL_sensor.h>
+
+//==================================================
+// Foundational Headers - SDL (Platform Specific)
+//==================================================
+#include <SDL3/SDL_platform.h>
+#include <SDL3/SDL_platform_defines.h>
+
+//==================================================
+// Foundational Headers - SDL (Testing)
+//==================================================
+#include <SDL3/SDL_test.h>
+#include <SDL3/SDL_test_assert.h>
+#include <SDL3/SDL_test_common.h>
+#include <SDL3/SDL_test_compare.h>
+#include <SDL3/SDL_test_crc32.h>
+#include <SDL3/SDL_test_font.h>
+#include <SDL3/SDL_test_fuzzer.h>
+#include <SDL3/SDL_test_harness.h>
+#include <SDL3/SDL_test_log.h>
+#include <SDL3/SDL_test_md5.h>
+#include <SDL3/SDL_test_memory.h>
+
+//==================================================
+// Foundational Headers - SDL (Threading)
+//==================================================
+#include <SDL3/SDL_atomic.h>
+#include <SDL3/SDL_mutex.h>
+#include <SDL3/SDL_thread.h>
+
+//==================================================
+// Foundational Headers - SDL (Timing)
+//==================================================
+#include <SDL3/SDL_time.h>
+#include <SDL3/SDL_timer.h>
+
+//==================================================
+// Additional SDL Headers (Misc/Utilities)
+//==================================================
+#include <SDL3/SDL_dialog.h>
+#include <SDL3/SDL_endian.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_loadso.h>
+#include <SDL3/SDL_misc.h>
+#include <SDL3/SDL_oldnames.h>
+#include <SDL3/SDL_tray.h>
+
+//==================================================
+// Foundational Headers - OpenGL
+//==================================================
+#include <GL/gl.h>
