a5d3dfef57ee578e1b074269362624435a606d7d
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-08-08 18:37:32 -0700

    main.cpp
    So via Cocoa, SDL3 runs with the OpenGL renderer. Now, next, we will have to make sure that the Vulkan renderer does work, since that is a common alternative if you are running Linux.

--- main.cpp
+++ main.cpp
@@ -3,46 +3,46 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_events.h> 
 #include <SDL3/SDL_video.h>
-#include <SDL3/SDL_render.h>
+#include <GL/gl.h>
 
-int main(int argc, char* argv[]) 
-{
+int main(int argc, char* argv[]) {
 
-    SDL_Window *window;
+    SDL_Window *window;                    // Declare a pointer
     bool done = false;
 
-    SDL_Init(SDL_INIT_VIDEO); 
-    window = SDL_CreateWindow(
-        "An SDL3 window",
-        640,
-        480,
-        0 
-            ); 
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
 
-    
-    if (window == NULL) 
-    {
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL3 window",                  // window title
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         return 1;
     }
 
-    while (!done) 
-    {
+    while (!done) {
         SDL_Event event;
 
-        while (SDL_PollEvent(&event)) 
-        {
-            if (event.type == SDL_EVENT_QUIT) 
-            {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
                 done = true;
             }
         }
 
+        // Do game logic, present a frame, etc.
     }
 
+    // Close and destroy the window
     SDL_DestroyWindow(window);
 
+    // Clean up
     SDL_Quit();
-
     return 0;
 }
