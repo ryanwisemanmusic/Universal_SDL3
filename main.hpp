05dd8f2441212c85b397ca3fa9df93a305f8cbb6
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-08-08 19:14:10 -0700

    main.hpp
    Awesome, some of the basic SDL3 headers are initialized, whether they work in the project is another question.

---
+++ main.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h> 
+#include <SDL3/SDL_video.h>
+#include <GL/gl.h>
+
+//Headers - successful import 01
+#include <SDL3/SDL_assert.h>
+#include <SDL3/SDL_asyncio.h>
+#include <SDL3/SDL_atomic.h>
+#include <SDL3/SDL_audio.h>
+#include <SDL3/SDL_begin_code.h>
+
+//This is a testing ground for new headers
