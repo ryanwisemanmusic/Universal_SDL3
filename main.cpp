677b0b0f620460f4db0fc2be502b11fa5007fb23
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-08-30 03:36:47 -0700

    main.cpp
    A WINDOW HAS BEEN CREATED THROUGH XQUARTZ!!!!!!!!!!!!! THIS IS THE BIGGEST DEVELOPMENT SO FAR!!!!!!
    
    THAT MEANS I HAVE A GOOD BASELINE FOR THIS PROJECT, AND SO NOW WE FIX ALL NON FATAL ERRORS!!!!!!!!!
    
    Now, we sleep. I'm just glad that the bare minimum works, which is all that you need to then make even better progress

--- main.cpp
+++ main.cpp
@@ -7,49 +7,39 @@ using namespace std;
 int main(int argc, char* argv[]) 
 {
     SDL_Window *window;
-    
-    cout << "Hello there! :D";
-    return 0;
-}
-
-//First checkpoint will be isolating the SDL_Window *window, and see if we can point to it
-//It's fucking pointless if you can't do anything beyond that.
-/*
-SDL_Window *window;                    // Declare a pointer
     bool done = false;
+    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
-
-    // Create an application window with the following settings:
     window = SDL_CreateWindow(
-        "An SDL3 window",                  // window title
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_VULKAN                  // flags - see below
+        "An SDL3 window",
+        640,
+        480,
+        0
     );
 
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
+    if (window == NULL) 
+    {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         return 1;
     }
 
-    while (!done) {
+    while (!done) 
+    {
         SDL_Event event;
 
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
+        while (SDL_PollEvent(&event)) 
+        {
+            if (event.type == SDL_EVENT_QUIT) 
+            {
                 done = true;
             }
         }
 
-        // Do game logic, present a frame, etc.
     }
 
-    // Close and destroy the window
     SDL_DestroyWindow(window);
 
-    // Clean up
     SDL_Quit();
-*/
+    
+    return 0;
+}
